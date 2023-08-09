c815c9c5f7414bf32c1e27be1b494fe6620b66b7
Yury Getman <imblowfish@duck.com>
2023-08-09 23:36:05 +0400

    chore: fix `_404.tsx` formatting

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -1,4 +1,3 @@
-
 import { Head } from "$fresh/runtime.ts";
 
 export default function Error404() {
@@ -17,10 +16,13 @@ export default function Error404() {
             alt="the fresh logo: a sliced lemon dripping with juice"
           />
           <h1 class="text-4xl font-bold">404 - Page not found</h1>
-          <p class="my-4">
-            The page you were looking for doesn't exist.
-          </p>
-          <a href="/" class="underline">Go back home</a>
+          <p class="my-4">The page you were looking for doesn't exist.</p>
+          <a
+            href="/"
+            class="underline"
+          >
+            Go back home
+          </a>
         </div>
       </div>
     </>
