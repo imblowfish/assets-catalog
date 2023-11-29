a56ee3881754d77f24b45983525e81525f9a6ec8
Yury Getman <imblowfish@duck.com>
2023-11-29 23:08:20 +0400

    chore(ui): move `assets` to separated route

---
+++ routes/users/[username].tsx
@@ -0,0 +1,18 @@
+import { defineRoute } from "$fresh/src/server/defines.ts";
+import { Header } from "$/components/Header.tsx";
+import { Avatar } from "$/components/Avatar.tsx";
+
+export default defineRoute((_req, ctx) => {
+  const username = ctx.params.username;
+
+  return (
+    <>
+      <Header />
+      <main class="h-full flex flex-col items-center m-4">
+        <Avatar sx="w-72 h-72" />
+        <p class="text-2xl mt-4">Full Name</p>
+        <p class="text-lg">{username}</p>
+      </main>
+    </>
+  );
+});
