75a543581e765ef72e1d8107c3d652c14f6e7208
Yury Getman <imblowfish@duck.com>
2023-12-03 04:21:32 +0400

    chore: migrate from `twind` to `tailwind`

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -2,8 +2,22 @@ import { AppProps } from "$fresh/server.ts";
 
 export default function App({ Component }: AppProps) {
   return (
-    <>
-      <Component />
-    </>
+    <html>
+      <head>
+        <meta charset="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        />
+        <title>Asset Catalog</title>
+        <link
+          rel="stylesheet"
+          href="/styles.css"
+        />
+      </head>
+      <body>
+        <Component />
+      </body>
+    </html>
   );
 }
