6bb8a8c3a73f1a32f8f35c565925c9f51f93330c
Yury Getman <imblowfish@duck.com>
2023-12-06 23:06:56 +0400

    feat(ui): fix `Button` to make sure there are no wrap and add `on*` callbacks

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,14 +1,21 @@
 import { ComponentChildren, VNode } from "preact";
 
 interface ButtonProps {
-  children: ComponentChildren;
+  children?: ComponentChildren;
   startIcon?: VNode;
   endIcon?: VNode;
+  onFocus?: () => void;
+  onBlur?: () => void;
+  sx?: string;
 }
 
 export const Button = (props: ButtonProps) => {
   return (
-    <button class="h-8  border border-2 rounded border-black bg-white hover:bg-black hover:text-white flex justify-between gap-1">
+    <button
+      class={`h-8 whitespace-nowrap border-2 rounded border-black bg-white hover:bg-black hover:text-white flex justify-between gap-1 ${props.sx}`}
+      onFocus={props.onFocus}
+      onBlur={props.onBlur}
+    >
       <div class="h-full flex items-center ml-2">{props.startIcon}</div>
       <div class="h-full flex justify-center items-center">
         {props.children}
