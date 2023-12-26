f6b6a22e89af83a69586297eaa0dca4aed0586dc
Yury Getman <imblowfish@duck.com>
2023-12-27 03:48:55 +0400

    fix(ui): incorrect divider centering

--- components/Divider.tsx
+++ components/Divider.tsx
@@ -7,9 +7,9 @@ export interface DividerProps {
 export const Divider = (props: DividerProps) => {
   return (
     <h1 class="w-full text-gray-500 flex items-center">
-      <span class="grow border-b h-[50%] border-gray-400" />
+      <span class="grow border-b border-gray-400" />
       {props.children && <div class="ml-1 mr-1">{props.children}</div>}
-      <span class="grow border-b h-[50%] border-gray-400" />
+      <span class="grow border-b border-gray-400" />
     </h1>
   );
 };
