670a25b30b63ee0d4c8ac99292ba4f4624a6d250
Yury Getman <imblowfish@duck.com>
2023-12-27 03:50:06 +0400

    fix(ui): change custom props for `Button` with default `button` props with `forwardRef`

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,26 +1,26 @@
-import { ComponentChildren, VNode } from "preact";
+import { VNode } from "preact";
+import { ForwardedRef, HTMLAttributes, forwardRef } from "preact/compat";
 
-interface ButtonProps {
-  children?: ComponentChildren;
+export type ButtonProps = HTMLAttributes<HTMLButtonElement> & {
   startIcon?: VNode;
   endIcon?: VNode;
-  onFocus?: () => void;
-  onBlur?: () => void;
   sx?: string;
-}
-
-export const Button = (props: ButtonProps) => {
-  return (
-    <button
-      class={`h-8 whitespace-nowrap border-2 rounded border-black bg-white hover:bg-black hover:text-white flex justify-between gap-1 ${props.sx}`}
-      onFocus={props.onFocus}
-      onBlur={props.onBlur}
-    >
-      <div class="h-full flex items-center ml-2">{props.startIcon}</div>
-      <div class="h-full flex justify-center items-center">
-        {props.children}
-      </div>
-      <div class="h-full flex items-center mr-2">{props.endIcon}</div>
-    </button>
-  );
 };
+
+export const Button = forwardRef(
+  (props: ButtonProps, ref: ForwardedRef<HTMLButtonElement>) => {
+    return (
+      <button
+        ref={ref}
+        class={`h-8 whitespace-nowrap border rounded border-black bg-white hover:bg-black hover:text-white flex justify-between gap-1 ${props.sx}`}
+        {...props}
+      >
+        <div class="h-full flex items-center ml-2">{props.startIcon}</div>
+        <div class="h-full flex justify-center items-center">
+          {props.children}
+        </div>
+        <div class="h-full flex items-center mr-2">{props.endIcon}</div>
+      </button>
+    );
+  },
+);
